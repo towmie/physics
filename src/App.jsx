8c308aae16e0a107709d25ee77b1b0a132971405
towmie <andre00toz@gmail.com>
2024-09-05 11:50:31 +0300

    feat: add collider boxes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Canvas, useThree } from "@react-three/fiber";
 import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
 import { OrbitControls, OrthographicCamera } from "@react-three/drei";
 import { useMemo, useRef } from "react";
+import * as THREE from "three";
+const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
 
 function Cube({ position }) {
   const cubeRef = useRef();
@@ -10,11 +12,7 @@ function Cube({ position }) {
   };
   return useMemo(() => {
     return (
-      <RigidBody
-        ref={cubeRef}
-        position={position}
-        rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
-      >
+      <RigidBody ref={cubeRef} position={position}>
         <mesh onClick={handleClick}>
           <boxGeometry args={[2, 1]} />
           <meshStandardMaterial color="blue" />
@@ -27,27 +25,35 @@ function Cube({ position }) {
 function Floor() {
   const { viewport } = useThree();
   return (
-    <RigidBody type="fixed" position={[0, -viewport.height / 2, 0]}>
-      <mesh>
-        <boxGeometry args={[viewport.width, 0.01, 10]} />
-        <meshStandardMaterial color="green" />
-      </mesh>
-    </RigidBody>
-  );
-}
-
-function Walls() {
-  const { viewport } = useThree();
-  return (
     <>
-      <CuboidCollider
-        args={[0.5, viewport.height / 2, 0.5]}
-        position={[-viewport.width / 2, 0, 0]}
-      />
-      <CuboidCollider
-        args={[0.5, viewport.height / 2, 0.5]}
-        position={[viewport.width / 2, 0, 0]}
-      />
+      <RigidBody type="fixed" restitution={0.4} friction={0}>
+        {/* bottom */}
+        <CuboidCollider
+          type="fixed"
+          args={[viewport.width / 2, 0.01, 0.01]}
+          position={[0, -viewport.height / 2, 0]}
+        />
+        <CuboidCollider
+          type="fixed"
+          args={[viewport.width / 2, viewport.height / 2, 0.01]}
+          position={[0, 0, 1]}
+        />
+        <CuboidCollider
+          type="fixed"
+          args={[viewport.width / 2, viewport.height / 2, 0.01]}
+          position={[0, 0, -1]}
+        />
+        <CuboidCollider
+          type="fixed"
+          args={[0.01, viewport.height / 2, 0.01]}
+          position={[-viewport.width / 2, 0, 0]}
+        />
+        <CuboidCollider
+          type="fixed"
+          args={[0.01, viewport.height / 2, 0.01]}
+          position={[viewport.width / 2, 0, 0]}
+        />
+      </RigidBody>
     </>
   );
 }
@@ -60,12 +66,12 @@ function Scene() {
       <pointLight position={[10, 10, 10]} />
       <OrbitControls />
 
-      <Physics gravity={[0, -9.81, 0]}>
+      <Physics debug gravity={[0, -9.81, 0]}>
         {[...Array(5)].map((_, i) => (
           <Cube key={i} position={[i * 2 - 4, 5, 0]} />
         ))}
         <Floor />
-        <Walls />
+        {/* <Walls /> */}
       </Physics>
     </Canvas>
   );
