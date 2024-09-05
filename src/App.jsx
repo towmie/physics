09901df2f7620bdc1a3b840b4d3bdaaecb67d0b0
towmie <andre00toz@gmail.com>
2024-09-05 17:18:21 +0300

    feat: add round box, leva settings and materials

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,168 @@
 import { Canvas, useThree } from "@react-three/fiber";
 import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
-import { OrbitControls, OrthographicCamera } from "@react-three/drei";
+import {
+  MeshTransmissionMaterial,
+  OrbitControls,
+  OrthographicCamera,
+  RoundedBox,
+  Text3D,
+} from "@react-three/drei";
 import { useMemo, useRef } from "react";
-import * as THREE from "three";
-const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
+import { useControls } from "leva";
 
-function Cube({ position }) {
-  const cubeRef = useRef();
-  const handleClick = () => {
-    console.log("jump!");
-  };
-  return useMemo(() => {
-    return (
-      <RigidBody ref={cubeRef} position={position}>
-        <mesh onClick={handleClick}>
-          <boxGeometry args={[2, 1]} />
-          <meshStandardMaterial color="blue" />
-        </mesh>
+// const boxesInfo = [
+//   {
+//     // link:'https://www.google.com',
+//     text: "ya",
+//     scale: [4, 3, 0.8],
+//     position: [-4, 4, 0],
+//   },
+//   {
+//     // link:'https://www.google.com',
+//     text: "fb",
+//     scale: [8, 5, 0.8],
+//     position: [4, -4, 0],
+//   },
+//   {
+//     // link:'https://www.google.com',
+//     text: "Google",
+//     scale: [3, 3, 0.8],
+//     position: [0, 1, 0],
+//   },
+// ];
+function Card({ children, ...props }) {
+  const transmissionProps = useControls("Transmission Material", {
+    backside: true,
+    backsideThickness: { value: -0.25, min: -1, max: 1, step: 0.01 },
+    backsideRoughness: { value: 0.3, min: 0, max: 1, step: 0.01 },
+    envMapIntensity: { value: 0.25, min: 0, max: 1, step: 0.01 },
+    thickness: { value: 0.75, min: 0, max: 2, step: 0.01 },
+    roughness: { value: 0.01, min: 0, max: 1, step: 0.01 },
+    ior: { value: 2.2, min: 1, max: 3, step: 0.01 },
+    chromaticAberration: { value: 0.1, min: 0, max: 1, step: 0.01 },
+    clearcoat: { value: 0.2, min: 0, max: 1, step: 0.01 },
+    clearcoatRoughness: { value: 0.1, min: 0, max: 1, step: 0.01 },
+  });
+
+  return (
+    <>
+      <RigidBody restitution={0.4}>
+        <RoundedBox args={[2, 1, 0.1]} radius={0.1} scale={2} {...props}>
+          <MeshTransmissionMaterial {...transmissionProps} />
+        </RoundedBox>
+        {children}
       </RigidBody>
-    );
-  }, [position]);
+    </>
+  );
 }
 
 function Floor() {
   const { viewport } = useThree();
+
+  const colliderProps = useControls("Colliders", {
+    bottomY: { value: -viewport.height / 2, min: -20, max: 20, step: 0.1 },
+    frontBackZ: { value: 0.3, min: 0, max: 5, step: 0.1 },
+    leftRightX: { value: viewport.width / 2, min: 0, max: 20, step: 0.1 },
+  });
+
   return (
     <>
-      <RigidBody type="fixed" restitution={0.4} friction={0}>
-        {/* bottom */}
+      <RigidBody type="fixed" restitution={0.4}>
+        {/* Bottom collider */}
         <CuboidCollider
           type="fixed"
-          args={[viewport.width / 2, 0.01, 0.01]}
-          position={[0, -viewport.height / 2, 0]}
+          args={[viewport.width / 2, 0.01, 1]}
+          position={[0, colliderProps.bottomY, 0]}
         />
+        {/* Front collider */}
         <CuboidCollider
           type="fixed"
           args={[viewport.width / 2, viewport.height / 2, 0.01]}
-          position={[0, 0, 1]}
+          position={[0, 0, colliderProps.frontBackZ]}
         />
+        {/* Back collider */}
         <CuboidCollider
           type="fixed"
           args={[viewport.width / 2, viewport.height / 2, 0.01]}
-          position={[0, 0, -1]}
+          position={[0, 0, -colliderProps.frontBackZ]}
         />
+        {/* Left collider */}
         <CuboidCollider
           type="fixed"
-          args={[0.01, viewport.height / 2, 0.01]}
-          position={[-viewport.width / 2, 0, 0]}
+          args={[0.01, viewport.height / 2, 1]}
+          position={[-colliderProps.leftRightX, 0, 0]}
         />
+        {/* Right collider */}
         <CuboidCollider
           type="fixed"
-          args={[0.01, viewport.height / 2, 0.01]}
-          position={[viewport.width / 2, 0, 0]}
+          args={[0.01, viewport.height / 2, 1]}
+          position={[colliderProps.leftRightX, 0, 0]}
         />
       </RigidBody>
     </>
   );
 }
 
-function Scene() {
-  return (
-    <Canvas>
-      <OrthographicCamera makeDefault zoom={50} position={[0, 0, 5]} />
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <OrbitControls />
-
-      <Physics debug gravity={[0, -9.81, 0]}>
-        {[...Array(5)].map((_, i) => (
-          <Cube key={i} position={[i * 2 - 4, 5, 0]} />
-        ))}
-        <Floor />
-        {/* <Walls /> */}
-      </Physics>
-    </Canvas>
-  );
-}
-
 export default function App() {
-  return <Scene />;
+  const textProps = useControls("Text3D", {
+    size: { value: 0.5, min: 0.1, max: 2, step: 0.1 },
+    curveSegments: { value: 12, min: 1, max: 20, step: 1 },
+    bevelEnabled: true,
+    bevelSize: { value: 0.02, min: 0, max: 0.1, step: 0.001 },
+    bevelThickness: { value: 0.02, min: 0, max: 0.1, step: 0.001 },
+    bevelSegments: { value: 5, min: 1, max: 10, step: 1 },
+    height: { value: 0.2, min: 0, max: 1, step: 0.01 },
+  });
+
+  return (
+    <>
+      <Canvas>
+        <OrthographicCamera zoom={45} position={[0, -2, 5]} />
+        <OrbitControls />
+        <ambientLight intensity={Math.PI / 4} />
+        <spotLight
+          position={[0, 40, 26]}
+          angle={0.5}
+          decay={0.7}
+          distance={48}
+          penumbra={1}
+          intensity={1750}
+        />
+        <spotLight
+          color="white"
+          position={[20, -40, 26]}
+          angle={0.5}
+          decay={1}
+          distance={53}
+          penumbra={1}
+          intensity={2000}
+        />
+        <spotLight
+          color="red"
+          position={[15, 0, 20]}
+          angle={0.1}
+          decay={1}
+          distance={35}
+          penumbra={-1}
+          intensity={100}
+        />
+        {/* <SpotLightHelper /> */}
+
+        <Physics debug gravity={[0, -9.81, 0]}>
+          <Card>
+            <Text3D font="./helvetiker_regular.typeface.json" {...textProps}>
+              cv
+              <meshBasicMaterial color="white" />
+            </Text3D>
+          </Card>
+          <Floor />
+        </Physics>
+        {/* <Environment 
+          files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/peppermint_powerplant_2_1k.hdr"
+          background={true}
+          blur={1}
+        /> */}
+      </Canvas>
+    </>
+  );
 }
